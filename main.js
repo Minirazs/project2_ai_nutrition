005e2f45822bede2fd33bcaa2566b69812da1905
Rui Shan <ruishan.foo@gmail.com>
2020-09-11 23:09:39 +0800

    added image analyser code

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
+//1) Upload image - image analyser
+$(document).ready(function () {
+        
+  $("#submit").on("click", function (e) {
+      e.preventDefault();
+       
+      var form = new FormData();
+      const fileInput = document.querySelector('#file') ;//fileinput html id
+      form.append("file", fileInput.files[0]);
+      
+      var settings = {
+        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/images/analyze",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+          "x-rapidapi-key": "1b403f52a7964470963e3e30543d59da"
+        },
+        "processData": false,
+        "mimeType": "multipart/form-data",
+        "contentType": false,
+        "data": form
+      };//end settings
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+
+  });
+});//end jquery
 
 
-
-// Search recipe by name
+// 3) Search recipe by name
 let apiKey = "1b403f52a7964470963e3e30543d59da";
 let apiURL = "https://api.spoonacular.com/recipes/complexSearch";
 
@@ -29,10 +58,17 @@ $(function(){
       console.log(closestID);
 
       //2nd GET to display nutrition widget
-      let widgetURL = `https://api.spoonacular.com/recipes/${closestID}/nutritionWidget`
+      let widgetURL = `https://api.spoonacular.com/recipes/${closestID}/information`
       console.log(widgetURL)
 
-      axios.get(widgetURL).then(function (response) {
+      let params2 = {
+        "apiKey": apiKey,
+        "includeNutrition" : false
+      };
+
+      axios.get(widgetURL,{
+        "params": params2
+      }).then(function (response) {
         console.log(response);
         document.getElementById("nutrition").innerHTML = "Closest Recipe Search";
         document.getElementById("nutrition-widget").innerHTML = response;

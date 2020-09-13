be9873bce50dc2035aba30c45534ee67ce4990eb
Rui Shan <ruishan.foo@gmail.com>
2020-09-13 14:22:40 +0800

    css display

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
         console.log(responseJSON.nutrition.carbs.value);
 
         //nutrition values display
-        $(".nutrition-result").append(`<li>
+        $("#nutrition-result").append(`<li>
           <span class="category">CALORIES</span>
           <span class="value">${responseJSON.nutrition.calories.value}</span>
         </li>

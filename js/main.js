34c50ed32ea393a7b8478b41c198e1c262a60b44
Rui Shan <ruishan.foo@gmail.com>
2020-09-13 18:13:31 +0800

    added axios

--- js/main.js
+++ js/main.js
@@ -52,7 +52,9 @@ $(document).ready(function () {
         <li>
           <span class="category">CARBOHYDRATES</span>
           <span class="value">${responseJSON.nutrition.carbs.value}</span>
-        </li>`);
+        </li>
+        
+        <h4>Here are some interesting recipes you can try out! </h4>`);
 
         //similar recipes
         for (let i = 0; i < responseJSON.recipes.length; i++) {
@@ -96,27 +98,88 @@ $(function(){
     }).then(function (response) {
       console.log(response);
 
-      closestID = response.data.results[0].id;
+      //recipe search results
+      //let closestID = [];
+      //store the closest recipe ID from the search
+      let closestID = response.data.results[0].id;
       console.log(closestID);
-
-      //2nd GET to display nutrition widget
-      let widgetURL = `https://api.spoonacular.com/recipes/${closestID}/information`
-      console.log(widgetURL)
 
       let params2 = {
         "apiKey": apiKey,
-        "includeNutrition" : false
+        //"includeNutrition" : false
       };
 
-      axios.get(widgetURL,{
-        "params": params2
-      }).then(function (response) {
-        console.log(response);
-        document.getElementById("nutrition").innerHTML = "Closest Recipe Search";
-        document.getElementById("nutrition-widget").innerHTML = response;
-      })
+      document.getElementById("nutrition").innerHTML = "Recipe Search by text input";
 
-        //show all search results
+      //search by ID and display search information & use GET to retrieve info using ID
+      let widgetURL = `https://api.spoonacular.com/recipes/${closestID}/nutritionWidget.json`
+        
+        axios.get(widgetURL,{
+          "params": params2
+        }).then(function (response) {
+          console.log(response);
+          
+         //Display nutritional value of the recipe (closestID)
+         $("#nutrition-widget").append(`<li>
+          <span class="category">CALORIES</span>
+          <span class="value">${response.data.calories}</span>
+        </li>
+        <li>
+          <span class="category">FAT</span>
+          <span class="value">${response.data.fat}</span>
+        </li>
+        <li>
+          <span class="category">PROTEIN</span>
+          <span class="value">${response.data.protein}</span>
+        </li>
+        <li>
+          <span class="category">CARBOHYDRATES</span>
+          <span class="value">${response.data.carbs}</span>
+        </li>
+          
+        <h4>Here are some interesting recipes you can try out! </h4> `);
+        })
+
+        let params3 = {
+          "apiKey": apiKey,
+          "includeNutrition" : false
+        };
+
+        let recipeURL = `https://api.spoonacular.com/recipes/${closestID}/information`
+        
+        //show recipe information of the closestID with link to recipe
+        axios.get(recipeURL,{
+          "params": params3
+        }).then(function (response) {
+          console.log(response);
+          
+          $("#recipe-info").append(`<li class=''>
+           ${response.data.title} 
+           <a href="${response.data.sourceUrl}" target="_blank" alt="View Recipe!"><img src="${response.data.image}"></a></li>`);
+         
+        })
+
+        //find similar recipes to the closestID
+        let params4 = {
+          "apiKey": apiKey,
+          "includeNutrition" : false
+        };
+
+        let similarURL = `https://api.spoonacular.com/recipes/${closestID}/similar`
+        
+        //show recipe information of the closestID with link to recipe
+        axios.get(similarURL,{
+          "params": params4
+        }).then(function (response) {
+          console.log(response);
+          
+          $("#similar-info").append(`<li class=''>
+           <a href="${response.data.sourceUrl}" target="_blank"> ${response.data.title} </a></li>`);
+         
+        })
+
+
+        //show all search results 
         for (let recipe of response.data.results){
           $("#search-results").append(`<li class=''>
            ${recipe.id} 
